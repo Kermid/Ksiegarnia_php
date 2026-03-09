7a3141654a65bd0bd6ec7d20fe5b734830eb1233
Kermid <jakub.gibowski312@gmail.com>
2026-03-10 00:48:10 +0100

    update struktury plikow

--- Strony/panel_adm.php
+++ Strony/panel_adm.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style_adm.css">
+    <link rel="stylesheet" href="Css/style_adm.css">
     <title>Księgarnia</title>
 </head>
 <body>
